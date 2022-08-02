d686ce0cbe8f0bc61145a24f60cc8e029e6639dd
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-08-02 10:38:29 +0100

    feat/(services api) - Added spotify request and tested spotify api connection, as well as converted axios getToken to use fetch

--- src/config/CustomRoutes.tsx
+++ src/config/CustomRoutes.tsx
@@ -2,9 +2,14 @@ import * as React from "react";
 import { useRoutes } from "react-router-dom";
 
 import Home from "views/Home";
+import NotFound from "views/NotFound/NotFound";
 
 function CustomRoutes() {
     let element = useRoutes([
+        {
+            path: "*",
+            element: <NotFound />
+        },
         {
             path: "/",
             element: <Home />
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 
+import { getBrowseByID, getBrowseCategories } from "services/spotify/api/browse/browse";
 
 
 function Home() {
+
+    async function test() {
+        const res = await getBrowseCategories();
+        // const res2 = await getBrowseByID("toplists");
+
+        console.log(res)
+        // console.log(res2)
+    }
+
+    useEffect(() => {
+        test()
+    }, [])
+
     return (
         <div className="bg-cyan-500">
             Home
